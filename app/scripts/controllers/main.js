b7cde3557b83e6334cd8f589a0dc88ff976efbc7
larce <luis.arce22@gmail.com>
2017-03-08 17:21:18 -0400

    Format album items view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,12 +10,13 @@
 angular.module('spotifyExampleApp')
   .controller('MainCtrl', ['$scope', '$resource', '$rootScope', function ($scope, $resource, $rootScope) {
     $scope.searchAlbum = "";
-    $scope.result = [{name: "hello"}];
+    $scope.result = [];
     var Spotify = $resource('https://api.spotify.com/v1/search', {});
 
-    $scope.search = function() {
-      Spotify.get({q: $scope.searchAlbum, type: 'album'}, function(res) {
+    $scope.search = function(event) {
+      Spotify.get({q: $scope.searchAlbum , type: 'album'}, function(res) {
         $scope.result = res.albums.items;
+        console.log($scope.result[0]);
       });
     }
   }]);
